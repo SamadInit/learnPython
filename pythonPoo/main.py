8d7dd5c9cedfa9a3714cbfef9da014842b83299a
1abdessamad1 <daouiabdessamad41@gmail.com>
2025-12-12 23:03:36 +0100

    testing in the main class

--- pythonPoo/main.py
+++ pythonPoo/main.py
@@ -1,7 +1,14 @@
-class animal():
-    numberOfAnimals=0
+# this is the main class
+from animal import Animals
+print(" here are some theree diffrent animals :")
 
-    def __init__(self, name):
-        self.name=name
-        self.weight=0.0
-        
+
+a1=Animals("yane")
+a2=Animals("moriss")
+a3=Animals("jack")
+
+print(Animals.numberOfAnimals)
+
+print(a1.__str__())
+Animals.incNmbrOfAanimals()
+print(Animals.numberOfAnimals)
